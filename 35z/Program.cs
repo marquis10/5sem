8b3999ebb72c5b34f252362a33d3d8ba9911c126
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-17 13:29:35 +0500

    35z_last

--- 35z/Program.cs
+++ 35z/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-100, 101);
+}
+
+int SearchChisla(int[] array, int count = 0)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99)
+            count++;
+    }
+    return count;
+}
+int[] array = new int[123];
+InputArray(array);
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Количество элементов массива в диапазоне [10, 99] = {SearchChisla(array)}");
